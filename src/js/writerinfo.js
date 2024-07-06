bf8eef5f2eb2fdd6d923509189d7035e5a9c4554
getup700 <2609639898@qq.com>
2024-07-06 23:59:25 +0800

    成功获取webapi数据

--- src/js/writerinfo.js
+++ src/js/writerinfo.js
@@ -1,41 +1,37 @@
 
 var queryBtn = document.getElementById('queryWriterInfo');
 if(queryBtn){
-
+    queryBtn.addEventListener("click",function(event){
+        // alert('Button clicked!');
+        // 在这里放置你要执行的 JavaScript 代码
+        updateWriterInfo();
+    })
 }else{
     console.error('Element not found or is null.');
 }
 
-
-document.getElementById('button-container').addEventListener('click', 
-    function(event) {
-        if (event.target && event.target.id === 'queryWriterInfo') {
-            alert('Button clicked!');
-            // 在这里放置你要执行的 JavaScript 代码
-            updateWriterInfo();
-        }
-});
-
-
 function updateWriterInfo(){
     var xhr = new XMLHttpRequest();
-    xhr.open("get",'http://39.106.54.114:672/api/type/types',true);
 
-    xhr.onload = function(){
-        if(xhr.status>=200 && xhr.status<400){
-            var data = JSON.parse(xhr.responseText);
-            updateUIWithData(data);
-        }else{
-            console.error('Failure to load data;status:'+xhr.status);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                var data = JSON.parse(xhr.responseText);
+                updateUIWithData(data);
+            }else{
+                console.error('Failure to load data;status:'+xhr.status);
+            }
         }
     }
+    xhr.open("get",'http://39.106.54.114:672/api/type/types',true);
+    xhr.send();
 }
 
 function updateUIWithData(data){
     var tableBody = document.querySelector('#table-body');
     tableBody.innerHTML = '';
 
-    data.forEach(function(row) {
+    data.data.forEach(function(row) {
         var tr = document.createElement('tr');
         Object.values(row).forEach(function(value) {
             var td = document.createElement('td');
